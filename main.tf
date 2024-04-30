70cc5acc7c9345cfd0658163fec16cf0cd6898da
KeerthuK29 <163317070+KeerthuK29@users.noreply.github.com>
2024-04-30 12:26:19 +0530

    Delete main.tf

--- main.tf
+++
@@ -1,8 +0,0 @@
-resource "aws_instance" "jenkins_ec2_instance" {
-    ami           = "ami-04e5276ebb8451442" 
-    instance_type = "t2.micro"
-    tags = {
-    Name = "MyJenkinsInstance"
-  }
-  
-}
